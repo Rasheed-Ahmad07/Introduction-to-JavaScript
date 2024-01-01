e0d6b575f9e573bd7ce05e26986103cc96b7da3c
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-01 23:55:49 +0500

    Add files via upload

---
+++ 001 introduction/index.js
@@ -0,0 +1,47 @@
+console.log("bilal");
+document.write("sadeeq");
+
+
+// let 
+// var
+// const
+
+
+// var x= "ali";
+// console.log(x);
+
+
+// var bilal= " my name is bilal from hangu ";
+// console.log(bilal);
+// console.log("bilal");
+
+// var x=3;
+// var x=5;
+//     x=6;
+// console.log(x);
+
+
+
+// let y= 5;
+//  let y=6;   //not ALLOWED
+//  y=7;
+// console.log(y);
+
+
+// const z=4;
+// const z=5;
+// z=8;
+
+
+
+//let.... block scope 
+//var .....function scope
+
+
+let x=2;
+{
+    let x=4;
+console.log(x);
+
+}
+// console.log(x);
